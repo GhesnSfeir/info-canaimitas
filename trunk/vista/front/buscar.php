9bb1e63df6396a892c6c3cfba327a8fea22dbce4
gslivedt@gmail.com <gslivedt@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-01-15 02:58:57 +0000

    Primer commit: estructura del proyecto y primeros archivos de vista

---
+++ trunk/vista/front/buscar.php
@@ -0,0 +1,40 @@
+<h1 class="text-center">Buscar recursos</h1>
+<form class="form-inline" role="form">
+    <div class="form-group">
+        <label for="grado">Grado:</label>
+        <select class="form-control" id="grado" name="grado">
+            <option>Todos</option>
+            <option>1ero</option>
+            <option>2do</option>
+            <option>3ero</option>
+            <option>4to</option>
+            <option>5to</option>
+            <option>6to</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="asignatura">Asignatura:</label>
+        <input type="text" class="form-control" id="asignatura" placeholder="asignatura">
+    </div>
+    <div class="form-group">
+        <label for="tema">Tema:</label>
+        <input type="text" class="form-control" id="tema" placeholder="tema">
+    </div>
+    <div class="form-group">
+        <label for="tipo">Tipo:</label>
+        <select class="form-control" id="tipo" name="tipo">
+            <option>Cualquiera</option>
+            <option>Video</option>
+            <option>PDF</option>
+            <option>JClic</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="autor">Autor:</label>
+        <input type="text" class="form-control" id="autor" placeholder="autor">
+    </div>
+    <div class="form-group">
+        <br>
+        <button type="submit" class="btn btn-default">Buscar</button>
+    </div>
+</form>
