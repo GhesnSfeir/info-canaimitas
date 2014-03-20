431d279902ef73091de60877180a5131770ed270
gslivedt@gmail.com <gslivedt@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-03-20 20:50:59 +0000

    Actualizaciones css y de espaciado

--- vista/back/inicio.php
+++ vista/back/inicio.php
@@ -1,24 +1,25 @@
-<div>
-    <div class="row text-center">
-        <div class="col-sm-4">
-            <img src="img/faq.png" alt="" class="img-rounded">
-        </div>
-        <div class="col-sm-4">
-            <img src="img/recursos.png" alt="" class="img-rounded">
-        </div>
-        <div class="col-sm-4">
-            <img src="img/usuarios.png" alt="" class="img-rounded">
-        </div>
+<br>
+<br>
+<br>
+<div class="row text-center">
+    <div class="col-sm-4">
+        <img src="img/faq.png" alt="" class="img-rounded">
     </div>
-    <div class="row">
-        <div class="col-sm-4 text-center">
-            <h3>Preguntas frecuentes</h3>
-        </div>
-        <div class="col-sm-4 text-center">
-            <h3>Recursos</h3>
-        </div>
-        <div class="col-sm-4 text-center">
-            <h3>Usuarios</h3>
-        </div>
+    <div class="col-sm-4">
+        <img src="img/recursos.png" alt="" class="img-rounded">
+    </div>
+    <div class="col-sm-4">
+        <img src="img/usuarios.png" alt="" class="img-rounded">
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-4 text-center">
+        <h3>Preguntas frecuentes</h3>
+    </div>
+    <div class="col-sm-4 text-center">
+        <h3>Recursos</h3>
+    </div>
+    <div class="col-sm-4 text-center">
+        <h3>Usuarios</h3>
     </div>
 </div>
