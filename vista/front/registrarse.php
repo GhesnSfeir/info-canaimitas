8f1c720899f9718bacce500f2f0b10399d9f9c57
gslivedt@gmail.com <gslivedt@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-03-20 20:18:30 +0000

    CSS de inputs medianos y largos y parte del Registrar del front

--- vista/front/registrarse.php
+++ vista/front/registrarse.php
@@ -1,3 +1,37 @@
-<<<<<<< .mine
-<h1>SOY UNA LOFIA novia de un Lofio</h1>=======
-<h1>Registrarse</h1>>>>>>>> .r3
+<h1 class="text-center">Registrarse</h1>
+<div class="row">
+    <div class="col-md-3"></div>
+    
+    <div class="col-md-6">
+        <form class="form-horizontal">
+            
+           <div class="form-group">
+                <label for="nombre" class="col-md-5 control-label">Nombre: </label>
+                <input type="text" id="nombre" name="nombre" class="form-control mediano">
+            </div>
+            
+            <div class="form-group">
+                <label for="apellido" class="col-md-5 control-label">Apellido: </label>
+                <input type="text" id="apellido" name="apellido" class="form-control mediano">
+            </div>
+            
+            <div class="form-group">
+                <label for="email" class="col-md-5 control-label">E-mail: </label>
+                <input type="text" id="email" name="email" class="form-control mediano">
+            </div>
+            <div class="form-group">
+                <label for="sexo" class="col-md-5 control-label">Sexo: </label>
+                <select id="sexo" name="sexo" class="form-control mediano">
+                    <option>Femenino</option>
+                    <option>Masculino</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="fecha_nacimiento" class="col-md-5 control-label">Fecha de nacimiento: </label>
+                <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" class="form-control mediano">
+            </div>
+        </form>
+    </div>
+    
+    <div class="col-md-3"></div>
+</div>
