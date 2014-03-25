d90d92ab1af1a84130e67343974e7bb747ddce25
gslivedt@gmail.com <gslivedt@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-03-25 16:32:24 +0000

    Avances en comentarios_y_recomendaciones_recurso.php en el front

--- trunk/vista/front/comentarios_y_recomendaciones_recurso.php
+++ trunk/vista/front/comentarios_y_recomendaciones_recurso.php
@@ -26,8 +26,31 @@
         </form>
         
         <br>
-        
-        
+        <label>Comentarios </label>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <label>Daniela Montemarano:</label>
+                <p>Excelente material! La din&aacute;mica n&uacute;mero dos fue 
+                    perfecta para mi curso</p>
+                <div class="text-right">
+                    <a>Responder</a>
+                    |
+                    <a>Denunciar</a>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <label>Mimia Lo:</label>
+                <p>En mi curso, us&eacute; el material seg&uacute;n la 
+                    recomendaci&oacute;n n&uacute;mero 1, pero con una 
+                    variaci&oacute;n: lectura dramatizada. Funcion&oacute; 
+                    de maravilla!</p>
+                <div class="text-right">
+                    <a>Responder</a>
+                    |
+                    <a>Denunciar</a>
+                </div>
+            </li>
+        </ul>
     </div>
     <div class="col-md-1"></div>
 </div>
