41dd61ffb1523940a2d51562e034bccf1de29bef
batr.26@gmail.com <batr.26@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-04-18 03:02:12 +0000

    modificacion de los estilos del back

--- trunk/vista/back/olvido_contrasena_paso1.php
+++ trunk/vista/back/olvido_contrasena_paso1.php
@@ -1,36 +1,38 @@
 <h1 class="text-center">&#191;Olvid&oacute; su contrase&ntilde;a?</h1> 
-<div class="col-md-3"></div>
 
-<div class="col-md-6">
+<!--Dos columnas de margen (una de cada lado)-->
+<div class="col-md-8 col-md-offset-2"> 
     <form class="form-horizontal" role="form"> 
-        <div class="form-group">
-            <p class="text-justify">Paso 1 de 3: ¿Olvidó su contraseña? Escriba a
-               continuación la dirección de correo electrónico con la que fue registrado y le 
-               enviaremos las instrucciones para su restablecimiento.</p>
-        </div>
-        
-        <div class="form-group">
-            <label for="correo" class="col-md-offset-1 col-md-4">Introduzca su Correo: </label>
-            <input type="email" id="correo" name="correo" class="col-md-7 form-control mediano">
-        </div>
-        
-        <div class="form-group">
-            <div class="col-md-offset-1 col-md-11">
-                <label for="captcha"> Escriba los caracteres que aparecen en la im&aacute;gen</label>
+        <p class="text-justify">Paso 1 de 3: ¿Olvidó su contraseña? Escriba a
+           continuación la dirección de correo electrónico con la que fue registrado y le 
+           enviaremos las instrucciones para su restablecimiento.</p>
+     <div class="col-md-10 col-md-offset-1"> 
+        <div class="form-group">  
+            <div class="col-md-6">
+                <label for="correo" class="control-label">Introduzca su Correo: </label>
+            </div>
+            <div class="col-md-6">
+                <input type="email" id="correo" name="correo" class="form-control">
             </div>
         </div>
-		
+        
         <div class="form-group">
-            <div class="col-md-offset-1 col-md-11">
+            <div class="col-md-6">
+                <label for="captcha"> Escriba los caracteres de la im&aacute;gen</label>
+            </div>
+            <div class="col-md-6">
+                 <input type="text" id="captcha" name="captcha" class="form-control">
+                
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <div class="col-md-6">
                 <img alt="100x100" src="http://lorempixel.com/140/140/" />
             </div>
         </div>
-        
-        <div class="form-group">
-            <div class="col-md-offset-1 col-md-11">
-                <input type="text" id="captcha" name="captcha" class="form-control mediano">
-            </div>
-        </div>
+         
+     </div>
 
         <div class="form-group">
             <div class="col-md-12 text-right">
@@ -38,7 +40,4 @@
             </div>
         </div>     
     </form>
-</div>
-
-<div class="col-md-3">
 </div>
--- trunk/vista/back/olvido_contrasena_paso3.php
+++ trunk/vista/back/olvido_contrasena_paso3.php
@@ -1,51 +1,59 @@
 <h1 class="text-center">&#191;Olvid&oacute; su contrase&ntilde;a?</h1>
-<div class="col-md-1"></div>
 
-<div class="col-md-10">
+<!--Dos columnas de margen (una de cada lado)-->
+<div class="col-md-8 col-md-offset-2"> 
     <form class="form-horizontal" role="form"> 
-        <div class="form-group">
-            <p class="text-justify">Paso 2 de 3: Le hemos enviado un correo electr&oacute;nico con todo lo que necesita
-               para restablecer su contrase&ntilde;a. Si no logra visualizar su correo revise su 
-               bandeja de correo no deseado.</p>
-        </div>
-        
-        <div class="col-md-7">
+        <p class="text-justify">Paso 2 de 3: Le hemos enviado un correo electr&oacute;nico con todo lo que necesita
+           para restablecer su contrase&ntilde;a. Si no logra visualizar su correo revise su 
+           bandeja de correo no deseado.</p>
+
+        <div class="col-md-8">
             <div class="form-group">
-                <label for="contrasenaTemporal" class="col-md-5 control-label">Introduzca su Contrase&ntilde;a Temporal: </label>
-                <input type="password" id="contrasenaTemporal" name="contrasenaTemporal" class="form-control">
-            </div>
-            
-            <div class="form-group">
-                <label for="nuevacontrasena" class="col-md-5 control-label">Introduzca su Nueva Contrase&ntilde;a: </label>
-                <input type="password" id="nuevacontrasena" name="nuevacontrasena" class="form-control mediano">
-            </div>
-            
-            <div class="form-group">
-                <label for="repeticionNuevaContrasena" class="col-md-5 control-label">Repita su Nueva Contrase&ntilde;a: </label>
-                <input type="password" id="repeticionNuevaContrasena" name="repeticionNuevaContrasena" class="form-control mediano">
-            </div>
-           
-        </div>
-    
-        <div class="col-md-5">
-            <div class="form-group">
-                <p class="text-justify">&#191;C&oacute;mo hacer su contrase&ntilde;a m&aacute;s segura?</p>
-                <div id="D_ListaPasosContrasenaSegura">
-                    <ul>
-                        <li>Usa un m&iacute;nimo de 6 caracteres</li>
-                        <li>Mezcle letras y n&uacute;meros</li>
-                        <li>Mezcle may&uacute;sculas y min&uacute;sculas</li>
-                        <li>Usa caracteres especiales (p.e. @) o espacios</li>
-                    </ul>
-                </div> 
-            </div>
-        
-            <div class="form-group">
-                <div class="col-md-12 text-right">
-                    <button type="submit" class="btn btn-default">Aceptar</button>            
+                <div class="col-md-6 text-right">
+                    <label for="contrasenaTemporal" class="control-label">Contrase&ntilde;a Temporal: </label>
                 </div>
-            </div>  
-        </div>         
-</div>
+                <div class="col-md-6">
+                    <input type="password" id="contrasenaTemporal" name="contrasenaTemporal" class="form-control">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-6 text-right">
+                    <label for="nuevacontrasena" class="control-label">Nueva Contrase&ntilde;a: </label>
+                </div>
+                <div class="col-md-6">
+                    <input type="password" id="nuevacontrasena" name="nuevacontrasena" class="form-control">               
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-6 text-right">
+                    <label for="repeticionNuevaContrasena" class="control-label">Repita su Nueva Contrase&ntilde;a: </label>
+                </div>
+                <div class="col-md-6">
+                    <input type="password" id="repeticionNuevaContrasena" name="repeticionNuevaContrasena" class="form-control">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <p class="text-justify">&#191;C&oacute;mo hacer su contrase&ntilde;a m&aacute;s segura?</p>
+            <div id="D_ListaPasosContrasenaSegura">
+                <ul>
+                    <li>Usa un m&iacute;nimo de 6 caracteres</li>
+                    <li>Mezcle letras y n&uacute;meros</li>
+                    <li>Mezcle may&uacute;sculas y min&uacute;sculas</li>
+                    <li>Usa caracteres especiales (p.e. @) o espacios</li>
+                </ul>
+            </div> 
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-12 text-right">
+                <button type="submit" class="btn btn-default">Aceptar</button>            
+            </div>
+        </div>          
+        
+        
     </form>
-<div class="col-md-1"></div>
+</div>
