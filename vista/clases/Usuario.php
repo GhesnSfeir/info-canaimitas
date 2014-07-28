5e059a744b48f118790f8657d0205bc53fd0678c
gslivedt@gmail.com <gslivedt@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-07-28 01:52:12 +0000

    Construyendo clase Usuario

--- vista/clases/Usuario.php
+++ vista/clases/Usuario.php
@@ -12,13 +12,14 @@ class Usuario {
     private $activo;
     
     
-    public function __construct($nombre, $email, $clave, $tipo) {
+    public function __construct($nombre, $email, $clave, $tipo, $id = null) {
         
         $this->nombre = $nombre;
         $this->email = $email;
         $this->clave = $clave;
         $this->tipo = $tipo;
         $this->activo = '1';
+        $this->id = $id;
         
     }
     
@@ -39,7 +40,7 @@ class Usuario {
     
     public function Guardar() {
         
-        if (isset($this->id)) {
+        if (isset($this->id)) { //Si el usuario ya esta en la base de datos
             
             $query = "UPDATE usuarios SET nombre='$this->nombre', 
                         email='$this->email', clave='$this->clave', 
@@ -55,7 +56,7 @@ class Usuario {
             $conexion->cerrar();
             
         }
-        else {
+        else { //Si el usuario no esta en la base de datos
             
             $query = "INSERT INTO usuarios (nombre, email, clave, tipo, activo) 
                         VALUES ('$this->nombre', '$this->email', '$this->clave',
@@ -70,6 +71,72 @@ class Usuario {
             $conexion->cerrar();
         }
         
+    }
+    
+    public static function consultarPorId($id) {
+        
+        $query = "SELECT * from usuarios where id=$id";
+        
+        $conexion = new ConexionBD();
+        $conexion->abrir();
+        
+        $resultado = $conexion->correrQuery($query);
+        
+        $registro = mysql_fetch_array($resultado);
+        
+        if ($registro) {
+            
+            return new Usuario(
+                    $registro['nombre'],
+                    $registro['email'],
+                    $registro['clave'],
+                    $registro['tipo'],
+                    $registro['id']);
+        }
+        else {
+            
+            return $registro;
+            
+        }
+        
+        $conexion->cerrar();
+    }
+    
+    public static function consultarTodos() {
+        
+        $query = "SELECT * from usuarios";
+        
+        $conexion = new ConexionBD();
+        $conexion->abrir();
+        
+        $resultado = $conexion->correrQuery($query);
+        
+        $numeroRegistros = mysql_num_rows($resultado);
+        
+        if ($numeroRegistros) {
+            
+            $usuarios = array();
+            
+            while ($registro = mysql_fetch_array($resultado)){
+                
+                array_push($usuarios, new Usuario(
+                    $registro['nombre'],
+                    $registro['email'],
+                    $registro['clave'],
+                    $registro['tipo'],
+                    $registro['id']));
+                
+            }
+            
+            return $usuarios;
+        }
+        else {
+            
+            return false;
+            
+        }
+        
+        $conexion->cerrar();
     }
     
 }
