3c3f934ce943785e7895709a7791b33f3ff64924
gslivedt@gmail.com <gslivedt@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-08-10 02:37:07 +0000

    Implementando funcionalidad de consultar y desactivar usuarios

--- vista/back/js/ghesn.js
+++ vista/back/js/ghesn.js
@@ -2,3 +2,7 @@ function cargarContenido(archivo) {
 	$("#contenido").load(archivo);
         inicializarVariables();
 }
+
+String.prototype.empiezaCon = function (str){
+        return this.indexOf(str) == 0;
+};
--- vista/back/scripts/crear_nuevo_usuario.php
+++ vista/back/scripts/crear_nuevo_usuario.php
@@ -1,44 +1,10 @@
 <?php
 
 include_once "../../clases/Usuario.php";
-//include_once "validaciones.php";
 
-$email = filter_input(INPUT_GET, 'email', FILTER_SANITIZE_EMAIL);
-$nombreUsuario = filter_input(INPUT_GET, 'nombreUsuario', FILTER_SANITIZE_STRING);
-$clave = filter_input(INPUT_GET, 'clave', FILTER_SANITIZE_STRING);
-
-/*function validarDatos($email, $nombreUsuario, $clave) {
-    
-    $mensaje = "";
-    $mensaje = $mensaje . validarEmail($email);
-    $mensaje = $mensaje . validarNombreUsuario($nombreUsuario);
-    $mensaje = $mensaje . validarClave($clave);
-    
-    return $mensaje;
-}
-
-$mensajeErrores = validarDatos($email, $nombreUsuario, $clave);
-
-if ($mensajeErrores === "") {
-    
-    $usuario = new Usuario($nombreUsuario, $email, $clave, 'gestor');
-    
-    if ($usuario->Guardar()) {
-        
-        echo "¡Enhorabuena!\n\nEl usuario ha sido creado con éxito!";
-        
-    }
-    else {
-        
-        echo "No se pudo agregar el usuario";
-        
-    }
-}
-else {
-    
-    echo $mensajeErrores;
-    
-}*/
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$nombreUsuario = filter_input(INPUT_POST, 'nombreUsuario', FILTER_SANITIZE_STRING);
+$clave = filter_input(INPUT_POST, 'clave', FILTER_SANITIZE_STRING);
 
 try {
     
@@ -51,7 +17,7 @@ try {
     }
     else {
         
-        echo "No se pudo agregar el usuario";
+        echo "¡Error!\n\nNo se ha podido agregar el usuario";
         
     }
 }
