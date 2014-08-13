0c069ed878ae5c7d7da0cb96efe69db56bb52262
gslivedt@gmail.com <gslivedt@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-08-13 02:11:43 +0000

    Arreglando el menu del back

---
+++ trunk/vista/back/menu_gestor.php
@@ -0,0 +1,33 @@
+<li class="tab active">
+    <a onclick="clickNavMenu(this, 'preguntas_frecuentes.php')">
+        Preguntas frecuentes
+    </a>
+</li>
+<li>
+    <a onclick="clickNavMenu(this, 'recursos.php')">
+        Recursos
+    </a>
+</li>
+<li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown">
+        Mi cuenta 
+        <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu text-left">
+        <li>
+            <a onclick="cerrarSesion()">
+                Cerrar sesi&oacute;n
+            </a>
+        </li>
+        <li>
+            <a onclick="cargarContenido('gestionar_cuenta_usuario.php')">
+                Gestionar
+            </a>
+        </li>
+    </ul>
+</li>
+<li>
+    <a onclick="clickNavMenu(this, 'ayuda.php')">
+        Ayuda
+    </a>
+</li>
