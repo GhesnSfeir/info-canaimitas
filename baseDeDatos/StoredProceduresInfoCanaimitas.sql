969376467ca5b90964da6f270fa9a6be374f6dbe
batr.26@gmail.com <batr.26@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-08-14 16:11:58 +0000

    StoredProceduresInfoCanaimitas.sql probado :)

--- baseDeDatos/StoredProceduresInfoCanaimitas.sql
+++ baseDeDatos/StoredProceduresInfoCanaimitas.sql
@@ -42,6 +42,14 @@ BEGIN
     SELECT * FROM preguntas_frecuentes;
 END // 
 
+-- Procedimiento para consultar preguntas frecuentes por id
+DELIMITER // 
+DROP PROCEDURE IF EXISTS consultar_preguntas_frecuentes;
+CREATE  PROCEDURE consultar_preguntas_frecuentes (idPregunta INT) 
+BEGIN 
+    SELECT * FROM preguntas_frecuentes where id = idPregunta;
+END // 
+
 -- Procedimiento para agregar preguntas frecuentes
 DELIMITER // 									
 DROP PROCEDURE IF EXISTS agregar_preguntas_frecuentes; 
@@ -84,15 +92,15 @@ END //
 -- Procedimiento para agregar preguntas particulares
 DELIMITER // 									
 DROP PROCEDURE IF EXISTS agregar_preguntas_particulares; 
-CREATE  PROCEDURE agregar_preguntas_particulares (preguntaparticular VARCHAR(1000), revisadaparticular TINYINT) 		
+CREATE  PROCEDURE agregar_preguntas_particulares (preguntaparticular VARCHAR(1000), revisadaparticular TINYINT, idUsuario INT) 		
 BEGIN  										
-   INSERT INTO preguntas_particulares (pregunta, revisada) VALUES (preguntaparticular, revisadaparticular);
+   INSERT INTO preguntas_particulares (pregunta, revisada, fk_usuarios) VALUES (preguntaparticular, revisadaparticular, idUsuario);
 END //  
 
   -- Procedimiento para modificar preguntas particulares
 DELIMITER // 									
 DROP PROCEDURE IF EXISTS modificar_preguntas_particulares; 
-CREATE  PROCEDURE modificar_preguntas_particulares (idPreguntaParticular INT, preguntaparticular VARCHAR(1000), revisadaparticular TINYINT)) 		
+CREATE  PROCEDURE modificar_preguntas_particulares (idPreguntaParticular INT, preguntaparticular VARCHAR(1000), revisadaparticular TINYINT) 		
 BEGIN  	
 	UPDATE preguntas_particulares  SET
     pregunta = preguntaparticular, 
@@ -111,8 +119,6 @@ END //
 
 
 -- F I C H A  S   D E   L O S   R E C U R S O S
-
-id int, titulo VARCHAR(100), formato VARCHAR(10), ruta_acceso VARCHAR(1000), caracterizacion_url VARCHAR(1000), recurso_url VARCHAR(1000)
 
 -- Procedimiento para consultar todas las fichas de los recursos
 DELIMITER // 
@@ -133,7 +139,7 @@ END //
   -- Procedimiento para modificar las fichas de los recursos
 DELIMITER // 									
 DROP PROCEDURE IF EXISTS modificar_fichas_recursos; 
-CREATE  PROCEDURE modificar_fichas_recursos (idFichaRecurso, tituloRecurso VARCHAR(100), formatoRecurso VARCHAR(10), ruta_accesoRecurso VARCHAR(1000), caracterizacion_urlRecurso VARCHAR(1000), recurso_urlRecurso VARCHAR(1000)) 		
+CREATE  PROCEDURE modificar_fichas_recursos (idFichaRecurso INT, tituloRecurso VARCHAR(100), formatoRecurso VARCHAR(10), ruta_accesoRecurso VARCHAR(1000), caracterizacion_urlRecurso VARCHAR(1000), recurso_urlRecurso VARCHAR(1000)) 		
 BEGIN  	
 	UPDATE fichas_recursos  SET
     titulo = tituloRecurso, 
@@ -147,9 +153,148 @@ END //
   -- Procedimiento para eliminar las fichas de los recursos
 DELIMITER // 									
 DROP PROCEDURE IF EXISTS eliminar_preguntas_particulares; 
-CREATE  PROCEDURE eliminar_preguntas_particulares (idPreguntaFrecuente INT) 		
+CREATE  PROCEDURE eliminar_fichas_recursos (idFichaRecurso INT) 		
 BEGIN  	
-	DELETE FROM preguntas_particulares
-    WHERE id = idPreguntaFrecuente;							
+	DELETE FROM fichas_recursos
+    WHERE id = idFichaRecurso;							
 END //
 
+-- R E C O M E N D A C I O N E S
+
+-- Procedimiento para consultar todas las recomendaciones
+DELIMITER // 
+DROP PROCEDURE IF EXISTS consultar_recomendaciones;
+CREATE  PROCEDURE consultar_recomendaciones () 
+BEGIN 
+    SELECT * FROM recomendaciones;
+END // 
+
+-- Procedimiento para agregar recomendaciones
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS agregar_recomendaciones;
+CREATE  PROCEDURE agregar_recomendaciones (recomendacionRecurso	VARCHAR(40), idFichaRecurso INT) 		
+BEGIN  										
+   INSERT INTO recomendaciones (recomendacion, fk_fichas_recursos_reco) VALUES (recomendacionRecurso, idFichaRecurso);
+END //  
+
+  -- Procedimiento para modificar recomendaciones
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS modificar_recomendaciones; 
+CREATE  PROCEDURE modificar_recomendaciones (idRecomendacion INT, recomendacionRecurso	VARCHAR(40), idFichaRecurso INT) 		
+BEGIN  	
+	UPDATE recomendaciones  SET
+    recomendacion = recomendacionRecurso, 
+	fk_fichas_recursos_reco = idFichaRecurso
+    WHERE id = idRecomendacion;							
+END // 
+
+  -- Procedimiento para eliminar recomendaciones
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS eliminar_recomendaciones; 
+CREATE  PROCEDURE eliminar_recomendaciones (idRecomendacion INT) 		
+BEGIN  	
+	DELETE FROM recomendaciones
+    WHERE id = idRecomendacion;							
+END //
+
+-- A U T O R E S
+
+-- Procedimiento para consultar todas los autores
+DELIMITER // 
+DROP PROCEDURE IF EXISTS consultar_autores;
+CREATE  PROCEDURE consultar_autores () 
+BEGIN 
+    SELECT * FROM autores;
+END // 
+
+-- Procedimiento para agregar autores
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS agregar_autores;
+CREATE  PROCEDURE agregar_autores (nombreAutor VARCHAR(50)) 		
+BEGIN  										
+   INSERT INTO autores (nombre) VALUES (nombreAutor);
+END //  
+
+  -- Procedimiento para modificar autores
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS modificar_autores; 
+CREATE  PROCEDURE modificar_autores (idAutor INT, nombre VARCHAR(50)) 		
+BEGIN  	
+	UPDATE autores  SET
+    nombre = nombreAutor
+    WHERE id = idAutor;							
+END // 
+
+  -- Procedimiento para eliminar autores
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS eliminar_autores; 
+CREATE  PROCEDURE eliminar_autores(idAutor INT)
+BEGIN  	
+	DELETE FROM autores
+    WHERE id = idAutor;							
+END //
+
+-- A U T O R E  S   F I C H A S    R E C U R S O S
+
+-- Procedimiento para agregar autores fichas recursos
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS agregar_autores_fichas_recursos;
+CREATE  PROCEDURE agregar_autores_fichas_recursos (idAutor INT, idFichaRecurso INT) 		
+BEGIN  										
+   INSERT INTO autores_fichas_recursos (fk_autores, fk_fichas_recursos_auto) VALUES (idAutor, idFichaRecurso);
+END //  
+ 
+  -- Procedimiento para eliminar autores fichas recursos
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS eliminar_autores_fichas_recursos; 
+CREATE  PROCEDURE eliminar_autores_fichas_recursos(idAutorFR INT)
+BEGIN  	
+	DELETE FROM autores_fichas_recursos
+    WHERE id = idAutorFR;							
+END //
+
+-- P E R I O D O S   A C A D E M I C O S
+
+-- Procedimiento para consultar todas los periodos academicos
+DELIMITER // 
+DROP PROCEDURE IF EXISTS consultar_preguntas_frecuentes;
+CREATE  PROCEDURE consultar_preguntas_frecuentes () 
+BEGIN 
+    SELECT * FROM preguntas_frecuentes;
+END // 
+
+-- Procedimiento para consultar los periodos academicos
+DELIMITER // 
+DROP PROCEDURE IF EXISTS consultar_preguntas_frecuentes;
+CREATE  PROCEDURE consultar_preguntas_frecuentes (idPregunta INT) 
+BEGIN 
+    SELECT * FROM preguntas_frecuentes where id = idPregunta;
+END // 
+
+-- Procedimiento para agregar los periodos academicos
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS agregar_preguntas_frecuentes; 
+CREATE  PROCEDURE agregar_preguntas_frecuentes (preguntaFrecuente VARCHAR(1000), respuestaFrecuente VARCHAR(1000)) 		
+BEGIN  										
+   INSERT INTO preguntas_frecuentes (pregunta, respuesta) VALUES (preguntaFrecuente, respuestaFrecuente);
+END //  
+
+  -- Procedimiento para modificar los periodos academicos
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS modificar_preguntas_frecuentes; 
+CREATE  PROCEDURE modificar_preguntas_frecuentes (idPreguntaFrecuente INT, preguntaFrecuente VARCHAR(1000), respuestaFrecuente VARCHAR(1000)) 		
+BEGIN  	
+	UPDATE preguntas_frecuentes  SET
+    pregunta = preguntaFrecuente, 
+	respuesta = respuestaFrecuente
+    WHERE id = idPreguntaFrecuente;							
+END // 
+
+  -- Procedimiento para eliminar los periodos academicos
+DELIMITER // 									
+DROP PROCEDURE IF EXISTS eliminar_preguntas_frecuentes; 
+CREATE  PROCEDURE eliminar_preguntas_frecuentes (idPreguntaFrecuente INT) 		
+BEGIN  	
+	DELETE FROM preguntas_frecuentes
+    WHERE id = idPreguntaFrecuente;							
+END //
