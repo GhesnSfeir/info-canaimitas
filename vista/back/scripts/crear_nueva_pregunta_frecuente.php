9cf3062c1fe899c1c2df041b5d5a2adf2d132642
gslivedt@gmail.com <gslivedt@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-08-19 17:20:36 +0000

    Implementando la funcion de crear nueva pregunta frecuente

---
+++ vista/back/scripts/crear_nueva_pregunta_frecuente.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once "../clases/PreguntaFrecuente.php";
+
+$pregunta = filter_input(INPUT_POST, 'pregunta', FILTER_SANITIZE_STRING);
+$respuesta = filter_input(INPUT_POST, 'respuesta', FILTER_SANITIZE_STRING);
+
+try {
+    
+    $preguntaFrecuente = new PreguntaFrecuente($pregunta, $respuesta);
+    
+    if ($preguntaFrecuente->Guardar()) {
+        
+        echo "¡Enhorabuena!\n\nLa pregunta frecuente ha sido creada con éxito.";
+        
+    }
+    else {
+        
+        echo "¡Error!\n\nNo se ha podido crear la pregunta frecuente.";
+        
+    }
+}
+catch (Exception $e) {
+    
+    echo $e->getMessage();
+    
+}
+
+?>
