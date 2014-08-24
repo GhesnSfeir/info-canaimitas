fe0969d2c5b7ae526f1bf394520087cc46938b4a
batr.26@gmail.com <batr.26@gmail.com@6dbea9c5-71c0-2349-578a-c877c49f38bc>
2014-08-24 08:31:29 +0000

    InsertsInfoCanaimitas.sql Asignaturas

---
+++ baseDeDatos/InsertsInfoCanaimitas.sql
@@ -0,0 +1,20 @@
+-- I N S E R T S   D E   A S I G N A T U R A S
+
+INSERT INTO asignaturas (nombre) VALUES ("Matemática");
+INSERT INTO asignaturas (nombre) VALUES ("Física");
+INSERT INTO asignaturas (nombre) VALUES ("Química");
+INSERT INTO asignaturas (nombre) VALUES ("Ciencia y tecnología");
+INSERT INTO asignaturas (nombre) VALUES ("Lenguaje");
+INSERT INTO asignaturas (nombre) VALUES ("Geografía universal");
+INSERT INTO asignaturas (nombre) VALUES ("Historia universal");
+INSERT INTO asignaturas (nombre) VALUES ("Historia de Venezuela");
+INSERT INTO asignaturas (nombre) VALUES ("Inglés");
+INSERT INTO asignaturas (nombre) VALUES ("Ciencias naturales");
+INSERT INTO asignaturas (nombre) VALUES ("Artes plásticas");
+INSERT INTO asignaturas (nombre) VALUES ("Ciencias sociales");
+INSERT INTO asignaturas (nombre) VALUES ("Ciudadanía e identidad");
+INSERT INTO asignaturas (nombre) VALUES ("Salud");
+
+
+
+
